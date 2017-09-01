0e8e9d7093f6f89285abf2ddf066dff096e52dda
LinKun-Linux <linkun199011@gmail.com>
2017-09-01 17:06:14 +0800

    init.

---
+++ app/src/main/java/com/ustclin/max/ultimateuniverse/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ustclin.max.ultimateuniverse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
